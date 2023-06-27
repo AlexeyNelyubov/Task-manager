94ea78445a1ac9064a1f7d0316a0bae1ba6e8a64
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-06-27 20:02:40 +0300

    add dist

--- src/composable/checkLocalStorage.ts
+++
@@ -1,8 +0,0 @@
-
-export const checkLocalStorage = (LocalStorageKey:string, tasks:string[]):void => {
-    if (localStorage.getItem(LocalStorageKey)) {
-      for (let task of JSON.parse(localStorage.getItem(LocalStorageKey).split(","))) {
-        tasks.push(task);
-      }
-    }
-  };
--- src/composable/deleteTask.ts
+++
@@ -1,9 +0,0 @@
-
-export const deleteTask = (tasks:string[], task:string, LocalStorageKey:string):void => {
-    tasks.splice(tasks.indexOf(task), 1);
-    if (tasks.length) {
-        localStorage.setItem(LocalStorageKey, JSON.stringify(tasks));
-      } else {
-        localStorage.removeItem(LocalStorageKey);
-      }
-  };
